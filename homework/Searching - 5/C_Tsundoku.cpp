6558c7bb47504d67d71ea91d5f2c8b1ca560e164
saurabh2214 <saurabh221413@gmail.com>
2024-03-21 17:18:31 +0530

    commit

--- homework/Searching - 5/C_Tsundoku.cpp
+++ homework/Searching - 5/C_Tsundoku.cpp
@@ -1,9 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int countsum(vector<int>& arr,int val)
+{
+    int sum=0;
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]>val)
+            break;
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int cntbooks(vector<int>& arr,int val)
+{
+    int low=0,high=arr.size()-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid]<=val)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+    }
+    return low;
+}
+
 int NoOfBooksRead(int n,vector<int>& A,int m,vector<int>& B,int k)
 {
-    
+    int low=min(A[0],B[0]);
+    int high=max(A[n-1],B[m-1]),ans=0;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(countsum(A,mid)+countsum(B,mid)<=k)
+        {
+            ans=cntbooks(A,mid);
+            ans+=cntbooks(B,mid);
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+    }
+    return ans;
 }
 
 int main()
