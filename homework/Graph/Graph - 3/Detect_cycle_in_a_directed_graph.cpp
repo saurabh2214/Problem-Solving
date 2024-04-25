e558eae01f4a5aae12ea269361866c7a1ffcd736
saurabh2214 <saurabh221413@gmail.com>
2024-04-25 13:56:18 +0530

    commit

---
+++ homework/Graph/Graph - 3/Detect_cycle_in_a_directed_graph.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool dfs(vector<int> adj[],int index,vector<bool>& vis, vector<bool>& recSt)
+    {
+        vis[index]=true;
+        recSt[index]=true;
+        
+        for(auto node:adj[index])
+        {
+            if(vis[node]==false && dfs(adj,node,vis,recSt))
+                return true;
+            else if(recSt[node]==true)
+                return true;
+        }
+        recSt[index]=false;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<bool> vis(V,false);
+        vector<bool> recSt(V,false);
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i] && dfs(adj,i,vis,recSt))
+                return true;
+        }
+        return false;
+    }
